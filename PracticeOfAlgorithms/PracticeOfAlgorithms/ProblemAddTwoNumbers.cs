9cc41aa1e18d5f27729c20e4404b14df3b180909
RecsFox <recs1911@gmail.com>
2023-09-07 01:21:07 +0300

    AddTwoNumbers

---
+++ PracticeOfAlgorithms/PracticeOfAlgorithms/ProblemAddTwoNumbers.cs
@@ -0,0 +1,41 @@
+﻿namespace PracticeOfAlgorithms;
+
+public class ProblemAddTwoNumbers
+{
+	public class ListNode
+	{
+		public int val;
+		public ListNode next;
+
+		public ListNode(int val = 0, ListNode next = null)
+		{
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+	{
+		ListNode answer = new(0);
+		ListNode current = answer;
+
+		int remainder = 0;
+
+		while(l1 != null || l2 != null || remainder != 0)
+		{
+			int x = l1?.val ?? 0;
+			int y = l2?.val ?? 0;
+
+			int sum = remainder + x + y;
+			remainder = sum / 10;
+
+			current.next = new ListNode(sum % 10);
+			current = current.next;
+
+			l1 = l1?.next;
+			l2 = l2?.next;
+		}
+
+		return answer.next;
+	}
+}
---
+++ PracticeOfAlgorithms/PracticeOfAlgorithmsTest/ProblemAddTwoNumbersTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using PracticeOfAlgorithms;
+
+namespace PracticeOfAlgorithmsTest;
+
+public class ProblemAddTwoNumbersTests
+{
+	[Test]
+	public void AddTwoNumbers_Example1()
+	{
+		// Arrange
+		ProblemAddTwoNumbers.ListNode l1 = new(2, new ProblemAddTwoNumbers.ListNode(4, new ProblemAddTwoNumbers.ListNode(3)));
+		ProblemAddTwoNumbers.ListNode l2 = new(5, new ProblemAddTwoNumbers.ListNode(6, new ProblemAddTwoNumbers.ListNode(4)));
+		ProblemAddTwoNumbers solver = new();
+
+		// Act
+		ProblemAddTwoNumbers.ListNode result = solver.AddTwoNumbers(l1, l2);
+
+		// Assert
+		Assert.IsNotNull(result);
+		Assert.AreEqual(7, result.val);
+		Assert.IsNotNull(result.next);
+		Assert.AreEqual(0, result.next.val);
+		Assert.IsNotNull(result.next.next);
+		Assert.AreEqual(8, result.next.next.val);
+		Assert.IsNull(result.next.next.next);
+	}
+
+
+	[Test]
+	public void AddTwoNumbers_Example2()
+	{
+		// Arrange
+		ProblemAddTwoNumbers.ListNode l1 = new(9);
+		ProblemAddTwoNumbers.ListNode l2 = new(1);
+		ProblemAddTwoNumbers solver = new();
+		
+		ProblemAddTwoNumbers.ListNode l2T = l2;
+		for( int i = 0; i < 99; i++ )
+		{
+			l2T.next = new ProblemAddTwoNumbers.ListNode(9);
+			l2T = l2T.next;
+		}
+
+		ProblemAddTwoNumbers.ListNode result = solver.AddTwoNumbers(l1, l2);
+		// Assert
+
+		for( int i = 0; i < 100; i++ )
+		{
+			Assert.IsNotNull(result);
+			Assert.AreEqual(0, result.val);
+			result = result.next;
+		}
+
+		Assert.IsNotNull(result);
+		Assert.AreEqual(1, result.val);
+	}
+}
