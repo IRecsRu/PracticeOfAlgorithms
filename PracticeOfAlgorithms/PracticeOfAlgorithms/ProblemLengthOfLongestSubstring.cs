ec006f7234122f5d0ac377d7e733cfc44d0e5450
RecsFox <recs1911@gmail.com>
2023-09-07 18:41:50 +0300

    LengthOfLongestSubstring

---
+++ PracticeOfAlgorithms/PracticeOfAlgorithms/ProblemLengthOfLongestSubstring.cs
@@ -0,0 +1,46 @@
+﻿namespace PracticeOfAlgorithms;
+
+public class ProblemLengthOfLongestSubstring
+{
+	public int LengthOfLongestSubstring(string s)
+	{
+		if (string.IsNullOrEmpty(s)) 
+			return 0;
+
+		if(s.Length < 2)
+			return s.Length;
+
+		int maxLength = 0;
+		int start = 0;
+
+		List<char> currentLChars = new();
+
+		for(; start < s.Length; start++)
+		{
+			char current = s[start];
+			
+			if(currentLChars.Contains(current))
+			{
+				maxLength = MaxLength(currentLChars, maxLength);
+
+				int index  = currentLChars.IndexOf(current);
+				currentLChars.RemoveRange(0, index +1);
+			}
+			
+			currentLChars.Add(current);
+		}
+
+		maxLength = MaxLength(currentLChars, maxLength);
+		return maxLength;
+	}
+	
+	private static int MaxLength(List<char> currentLChars, int maxLength)
+	{
+		int length = currentLChars.Count;
+
+		if(maxLength < length)
+			maxLength = length;
+		
+		return maxLength;
+	}
+}
---
+++ PracticeOfAlgorithms/PracticeOfAlgorithmsTest/ProblemLengthOfLongestSubstringTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using PracticeOfAlgorithms;
+
+namespace PracticeOfAlgorithmsTest;
+
+[TestFixture]
+public class ProblemLengthOfLongestSubstringTests
+{
+	[Test]
+	public void TwoSum_Example1()
+	{
+		// Arrange
+		ProblemLengthOfLongestSubstring solver = new();
+		string s = "abcabcbb";
+
+		// Act
+		int result = solver.LengthOfLongestSubstring(s);
+
+		// Assert
+		Assert.AreEqual(3, result);
+	}
+	
+	[Test]
+	public void TwoSum_Example2()
+	{
+		// Arrange
+		ProblemLengthOfLongestSubstring solver = new();
+		string s = "bbbbb";
+
+		// Act
+		int result = solver.LengthOfLongestSubstring(s);
+
+		// Assert
+		Assert.AreEqual(1, result);
+	}
+	
+	[Test]
+	public void TwoSum_Example3()
+	{
+		// Arrange
+		ProblemLengthOfLongestSubstring solver = new();
+		string s = "pwwkew";
+
+		// Act
+		int result = solver.LengthOfLongestSubstring(s);
+
+		// Assert
+		Assert.AreEqual(3, result);
+	}
+	
+	[Test]
+	public void TwoSum_Example4()
+	{
+		// Arrange
+		ProblemLengthOfLongestSubstring solver = new();
+		string s = "au";
+
+		// Act
+		int result = solver.LengthOfLongestSubstring(s);
+
+		// Assert
+		Assert.AreEqual(2, result);
+	}
+}
