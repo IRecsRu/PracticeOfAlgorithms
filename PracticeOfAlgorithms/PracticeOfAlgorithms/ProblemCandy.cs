80b39d95b1e900902550757aa74e2f248011343f
RecsFox <recs1911@gmail.com>
2023-09-14 03:43:50 +0300

    Candy

---
+++ PracticeOfAlgorithms/PracticeOfAlgorithms/ProblemCandy.cs
@@ -0,0 +1,59 @@
+﻿namespace PracticeOfAlgorithms;
+
+public class ProblemCandy
+{
+	/*public int Candy(int[] ratings)
+	{
+		int n = ratings.Length;
+		int[] candies = new int[n];
+		Array.Fill(candies, 1);
+
+		for( int i = 1; i < n; i++ )
+		{
+			if(ratings[i] > ratings[i - 1])
+				candies[i] = candies[i - 1] + 1;
+		}
+
+		for( int i = n - 2; i >= 0; i-- )
+		{
+			int j = i + 1;
+			
+			if(ratings[i] > ratings[j])
+				candies[i] = Math.Max(candies[i], candies[j] + 1);
+		}
+
+		return candies.Sum();
+	}*/
+	
+	public int Candy(int[] ratings)
+	{
+		if (ratings == null || ratings.Length == 0)
+			return 0;
+
+		int ret = 1, up = 0, down = 0, peak = 0;
+
+		for (int i = 1; i < ratings.Length; i++)
+		{
+			if (ratings[i - 1] < ratings[i])
+			{
+				up++;
+				down = 0;
+				peak = up;
+				ret += 1 + up;
+			}
+			else if (ratings[i - 1] == ratings[i])
+			{
+				up = down = peak = 0;
+				ret += 1;
+			}
+			else
+			{
+				down++;
+				up = 0;
+				ret += 1 + down - (peak >= down ? 1 : 0);
+			}
+		}
+
+		return ret;
+	}
+}
---
+++ PracticeOfAlgorithms/PracticeOfAlgorithmsTest/ProblemCandyTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using NUnit.Framework;
+using PracticeOfAlgorithms;
+
+namespace PracticeOfAlgorithmsTest;
+
+[TestFixture]
+public class ProblemCandyTests
+{
+	[Test]
+	public void TwoSum_Example1()
+	{
+		// Arrange
+		ProblemCandy solver = new ProblemCandy();
+		int[] ratings = { 1,0,2 };
+
+		// Act
+		int result = solver.Candy(ratings);
+
+		// Assert
+		Assert.AreEqual(5, result);
+	}
+
+	[Test]
+	public void TwoSum_Example2()
+	{
+		// Arrange
+		ProblemCandy solver = new ProblemCandy();
+		int[] ratings = { 1,2,2 };
+
+		// Act
+		int result = solver.Candy(ratings);
+
+		// Assert
+		Assert.AreEqual(4, result);
+	}
+	
+	[Test]
+	public void TwoSum_Example3()
+	{
+		// Arrange
+		ProblemCandy solver = new ProblemCandy();
+		int[] ratings = {1,2,87,87,87,2,1};
+
+		// Act
+		int result = solver.Candy(ratings);
+
+		// Assert
+		Assert.AreEqual(13, result);
+	}
+	
+	[Test]
+	public void TwoSum_Example4()
+	{
+		// Arrange
+		ProblemCandy solver = new ProblemCandy();
+		int[] ratings = new int[20000];
+		Random random = new Random();
+		
+		for( int i = 0; i < ratings.Length; i++ )
+			ratings[i] = random.Next(0, 20000);
+
+		// Act
+		int result = solver.Candy(ratings);
+
+	}
+}
